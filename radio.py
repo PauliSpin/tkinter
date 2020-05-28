f2591c963f87166df7f936c3eb347c68bdb93d35
Chotalia <rchotalia@dxc.com>
2020-05-28 15:10:21 +0100

    Multiple radio button using for loop

--- radio.py
+++ radio.py
@@ -6,8 +6,22 @@ root = tk.Tk()
 root.title('Hello Python World')
 root.iconbitmap('Chili_Pepper_icon.ico')
 
-r = tk.IntVar()  # Define a tkinter variable, you can get and set these
-r.set('2')
+# r = tk.IntVar()  # Define a tkinter variable, you can get and set these
+# r.set('2')
+
+TOPPINGS = [
+    ('Pepperoni', 'Pepperoni'),
+    ('Cheese', 'Cheese'),
+    ('Mushroom', 'Mushroom'),
+    ('Onion', 'Onion'),
+]
+
+pizza = tk.StringVar()
+pizza.set('Pepperoni')
+
+for text, topping in TOPPINGS:
+    tk.Radiobutton(root, text=text, variable=pizza,
+                   value=topping).pack(anchor=tk.W)
 
 
 def clicked(value):
@@ -15,17 +29,17 @@ def clicked(value):
     myLabel.pack()
 
 
-tk.Radiobutton(root, text="Option 1", variable=r, value=1,
-               command=lambda: clicked(r.get())).pack()
-tk.Radiobutton(root, text="Option 2", variable=r, value=2,
-               command=lambda: clicked(r.get())).pack()
+# tk.Radiobutton(root, text="Option 1", variable=r, value=1,
+#                command=lambda: clicked(r.get())).pack()
+# tk.Radiobutton(root, text="Option 2", variable=r, value=2,
+#                command=lambda: clicked(r.get())).pack()
 
-myLabel = tk.Label(root, text=r.get())
-myLabel.pack()
+# myLabel = tk.Label(root, text=pizza.get())
+# myLabel.pack()
 
 
 myButton = tk.Button(root, text='Click Me!',
-                     command=lambda: clicked(r.get()))
+                     command=lambda: clicked(pizza.get()))
 myButton.pack()
 
 root.mainloop()
