ad6cc977c542afaafa05c58778da043706dd6254
Chotalia <rchotalia@dxc.com>
2020-05-28 16:35:30 +0100

    Added button to second window to clsoe

--- base.py
+++ base.py
@@ -6,10 +6,19 @@ root = tk.Tk()
 root.title('Hello Python World')
 root.iconbitmap('Chili_Pepper_icon.ico')
 
-top = tk.Toplevel()
-top.title('Second Window!')
-top.iconbitmap('Chili_Pepper_icon.ico')
-my_img = ImageTk.PhotoImage(Image.open('Amaryllis.png'))
-tk.Label(top, image=my_img).pack()
+
+def open():
+    # For some reason have to have this as local variables don't work.
+    global my_img
+    top = tk.Toplevel()
+    top.title('Second Window!')
+    top.iconbitmap('Chili_Pepper_icon.ico')
+    my_img = ImageTk.PhotoImage(Image.open('Amaryllis.png'))
+    tk.Label(top, image=my_img).pack()
+    tk.Button(top, text='Close Window', command=top.destroy).pack()
+
+
+tk.Button(root, text='Open second window', command=open).pack()
+
 
 tk.mainloop()
