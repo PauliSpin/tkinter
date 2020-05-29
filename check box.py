c6db04313b06430528ac05295d6300c96562c3e3
Chotalia <rchotalia@dxc.com>
2020-05-29 08:29:59 +0100

    String return values

--- check box.py
+++ check box.py
@@ -11,9 +11,13 @@ root.title('Hello Python World')
 root.iconbitmap('Chili_Pepper_icon.ico')
 root.geometry('400x400')
 
-var = tk.IntVar()
+# var = tk.IntVar() # Integer Variable
+var = tk.StringVar()
 
-c = tk.Checkbutton(root, text="Check this box!", variable=var)
+# c = tk.Checkbutton(root, text="Check this box!", variable=var)  # for Integer Variable
+c = tk.Checkbutton(root, text="Check this box!", variable=var,
+                   onvalue='On', offvalue='Off')  # for String Variable
+c.deselect()    # Have to have this as otherwise it's selected and doesn't work as expected
 c.pack()
 
 tk.Button(root, text='Show Selection', command=show).pack()
